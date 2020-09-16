2d701d3f36a6c7d11f5b58c54fd6eed5592fa3a4
ManaliWani <wanimanali98@gmail.com>
2020-09-16 09:05:10 +0000

    Modified in new branch

--- Helloworld.java
+++ Helloworld.java
@@ -4,6 +4,6 @@ class Helloworld
 {
 	public static void main(String args[])
 	{
-		System.out.println("Welcome to the git world");
+		System.out.println("Welcome to the git world of new branch");
 	}
 }
